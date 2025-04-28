e2d0a12627aa6485106666cf877ffd464b9b2f8e
Apolo Barua Apurbo <apolo.itnet@gmail.com>
2025-04-28 14:41:24 +0600

    PH-B11-Auth-Integration-Layouts-commit

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,10 +1,26 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import React from "react";
 import { Link } from "react-router";
+import { auth } from "../../firebase.init";
 
 const Register = () => {
   
   const handleRegister = (e) => {
     e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+
+    console.log(name, email, password);
+
+    createUserWithEmailAndPassword(auth, email, password)
+    .then(result =>{
+      console.log(result);
+    })
+    .catch(error => {
+      console.log(error);
+      
+    })
 
   };
 
@@ -13,11 +29,13 @@ const Register = () => {
     <div className="card bg-base-100 w-full mx-auto mt-10 max-w-sm shrink-0 shadow-2xl">
       <h1 className="text-3xl text-center font-bold">Register now!</h1>
       <div className="card-body">
-        <form className="fieldset">
+        <form onSubmit={handleRegister} className="fieldset">
+          <label className="label">Name</label>
+          <input type="text" name="name" className="input" placeholder="Name" />
           <label className="label">Email</label>
-          <input type="email" className="input" placeholder="Email" />
+          <input type="email" name="email" className="input" placeholder="Email" />
           <label className="label">Password</label>
-          <input type="password" className="input" placeholder="Password" />
+          <input type="password" name="password" className="input" placeholder="Password" />
           <div>
             <a className="link link-hover">Forgot password?</a>
           </div>
@@ -25,8 +43,8 @@ const Register = () => {
         </form>
         <p>
           New to this stie? Please{" "}
-          <Link to="/register" className="text-blue-400 underline">
-            Register
+          <Link to="/login" className="text-blue-400 underline">
+            Login
           </Link>{" "}
         </p>
       </div>
